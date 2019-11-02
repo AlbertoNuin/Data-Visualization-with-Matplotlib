69bd0c92bff6ce22aff896cb9351f5b533f728bb
Alberto Nuin <alberto_nuin@hotmail.com>
2019-11-02 00:01:36 +0000

    Add files via upload

---
+++ scatter_plot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 23:34:35 2019
+
+@author: alberto
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = 1024
+X = np.random.normal(0, 1, n)
+Y = np.random.normal(0, 1, n)
+
+# Scatter plot parameters
+# c: color determined by angle of (X,Y)
+# s: marker size
+# alpha: transparency
+
+plt.scatter(X, Y, c=np.angle(X+Y*1j), s=15, alpha=0.6)
+
+# Hide axis
+plt.axis('off')
+
+# Save figure using 72 dots per inch
+plt.savefig('scatter_plot.png', dpi=72)
+
+plt.show()
